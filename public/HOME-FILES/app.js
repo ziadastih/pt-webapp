3d0adf56d30c35ddf1eb76ddf16bb65f05efac8b
ziad astih <ziadastih12@outlook.com>
2022-09-21 21:48:24 +0300

    managed the errors for registration and login and the close btn and functionality

--- public/HOME-FILES/app.js
+++ public/HOME-FILES/app.js
@@ -1,10 +1,10 @@
 // ==============navigation btns selection =================
 const getStartedBtn = document.querySelector(".get-started-btn");
 const loginNavBtn = document.getElementById("login-nav-btn");
-const closeBtn = document.getElementById("close-btn");
+const closeBtns = document.querySelectorAll("#close-btn");
 // ==========sections selection ==============
 const registerContainer = document.querySelector(".register-container");
-const loginSection = document.querySelector(".login-container");
+const loginContainer = document.querySelector(".login-container");
 
 // ===========navigation event listener ==========
 
@@ -12,19 +12,37 @@ const loginSection = document.querySelector(".login-container");
 getStartedBtn.addEventListener("click", () => {
   registerContainer.classList.add("open-container");
 });
-
-closeBtn.addEventListener("click", (e) => {
-  let id = e.target.dataset.close;
-  let container = document.querySelector(`.${id}`);
-  container.classList.remove("open-container");
-});
 // ===============login-nav-btn  ==================
 loginNavBtn.addEventListener("click", () => {
-  navBar.classList.add("js-nav-bar");
   registerContainer.classList.remove("open-container");
-  loginSection.classList.add("login-translate");
+  loginContainer.classList.add("open-container");
+});
+// =============close btns for each container so we save lines of code
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", (e) => {
+    let id = e.target.dataset.close;
+    let container = document.querySelector(`.${id}`);
+    container.classList.remove("open-container");
+    console.log(container);
+  });
 });
 
+// ==========eye toggle for password hide/show case ====
+const togglePassword = document.querySelectorAll("#toggle-password");
+const passwords = document.querySelectorAll(".password");
+
+togglePassword.forEach((eyeBtn) => {
+  eyeBtn.addEventListener("click", () => {
+    passwords.forEach((password) => {
+      const type = password.getAttribute("type");
+      if (type === "password") {
+        password.setAttribute("type", "text");
+      } else {
+        password.setAttribute("type", "password");
+      }
+    });
+  });
+});
 // ========================== register/login btn ==================
 const registerBtn = document.querySelector(".sign-up-btn");
 const loginBtn = document.querySelector(".login-btn");
@@ -37,6 +55,16 @@ const registerPassword = document.getElementById("register-password");
 const loginEmail = document.getElementById("login-email");
 const loginPassword = document.getElementById("login-password");
 
+// ==============alert handling classes ===============================
+const registerFirstNameAlert = document.querySelector(".register-first-alert");
+const registerLastNameAlert = document.querySelector(".register-last-alert");
+const registerPasswordAlert = document.querySelector(
+  ".register-password-alert"
+);
+const registeremailAlert = document.querySelector(".register-email-alert");
+const loginPasswordAlert = document.querySelector(".login-password-alert");
+const loginEmailAlert = document.querySelector(".login-email-alert");
+
 // =========== connecting register btn and info to backend ======
 
 registerBtn.addEventListener("click", async (e) => {
@@ -45,7 +73,18 @@ registerBtn.addEventListener("click", async (e) => {
   const lastName = registerLastName.value;
   const email = registerEmail.value;
   const password = registerPassword.value;
-
+  if (firstName.length < 3) {
+    showAlert(registerFirstNameAlert);
+  }
+  if (lastName.length < 3) {
+    showAlert(registerLastNameAlert);
+  }
+  if (!validateEmail(email)) {
+    showAlert(registeremailAlert);
+  }
+  if (password.length < 6) {
+    showAlert(registerPasswordAlert);
+  }
   try {
     const { coach } = await axios.post("/api/v1/auth/register", {
       firstName,
@@ -53,8 +92,14 @@ registerBtn.addEventListener("click", async (e) => {
       email,
       password,
     });
-    loginSection.classList.add("login-translate");
-    registerSection.classList.remove("register-translate");
+    registerFirstName.value = "";
+    registerLastName.value = "";
+    registerEmail.value = "";
+    registerPassword.value = "";
+    setTimeout(() => {
+      loginContainer.classList.add("open-container");
+      registerContainer.classList.remove("open-container");
+    }, 500);
   } catch (error) {
     console.log(error);
   }
@@ -73,6 +118,22 @@ loginBtn.addEventListener("click", async (e) => {
     const coach = data.data;
     console.log(coach);
   } catch (error) {
-    console.log(error);
+    showAlert(loginEmailAlert);
+    showAlert(loginPasswordAlert);
   }
 });
+
+// =======================show alert function ===========================
+const showAlert = (alert) => {
+  alert.classList.add("show-alert");
+  setTimeout(() => {
+    alert.classList.remove("show-alert");
+  }, 3000);
+};
+
+// ==========validate email for regex  =================
+
+function validateEmail(emailValue) {
+  let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return res.test(emailValue);
+}
