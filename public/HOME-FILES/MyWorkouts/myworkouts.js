ae8d045290707cc88e25f2d7b39337fba043c276
ziad astih <ziadastih12@outlook.com>
2022-09-26 23:55:46 +0300

    done starting point for user when the workouts are empty , gave create btn , then in case he have programs , he will be able to see them with overview for the workouts , exercises , and info and can manage them quickly from here

---
+++ public/HOME-FILES/MyWorkouts/myworkouts.js
@@ -0,0 +1,395 @@
+const program = [
+  {
+    name: "push/pull",
+    createdAt: "22/9/2022",
+    updatedAt: "22/10/2022",
+    createdBy: "ziad10932diifasd",
+    workouts: [
+      {
+        name: "legs",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "pull",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "push",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "legs(hamstrings)",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "shoulders",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "biceps/triceps",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+// ===============toggle the program to get all the workouts =============
+const programContainer = document.querySelectorAll(".program-container");
+
+programContainer.forEach(function (program) {
+  const programArrow = program.querySelector("#show-program");
+
+  programArrow.addEventListener("click", function () {
+    programContainer.forEach(function (item) {
+      if (item === program) {
+        console.log(item);
+        item.classList.toggle("open-container");
+        programArrow.classList.toggle("rotate-arrow");
+      } else {
+        item.classList.remove("open-container");
+      }
+    });
+  });
+});
