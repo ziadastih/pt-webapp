1b2636c9243a1c1ab276d7c7b886d29768bcd767
ziad astih <ziadastih12@outlook.com>
2022-09-27 14:56:25 +0300

    created the html for the workout display and the function to display workouts and on click show and hide other containers , it is working fine and have a demo workout array to work with , next step is to add this array to database and fetch it when page load

--- public/HOME-FILES/MyWorkouts/myworkouts.js
+++ public/HOME-FILES/MyWorkouts/myworkouts.js
@@ -1,8 +1,382 @@
-const program = [
+const programPlan = [
   {
     name: "push/pull",
     createdAt: "22/9/2022",
     updatedAt: "22/10/2022",
+    createdBy: "ziad10932diifasd",
+    workouts: [
+      {
+        name: "legs",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extention",
+            sets: 4,
+            Reps: 11,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "abduction",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "pull",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "push",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "legs(hamstrings)",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "shoulders",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+      {
+        name: "biceps/triceps",
+        exercises: [
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "squats",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg press",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "bulgarian lunges",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "leg extension",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "lying leg curls",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "standing calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+          {
+            name: "seated calf raises",
+            sets: 4,
+            Reps: 15,
+            tempo: "3-1-1-0",
+            restTime: "90s",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "split",
+    createdAt: "22/11/2022",
+    updatedAt: "22/12/2022",
     createdBy: "ziad10932diifasd",
     workouts: [
       {
@@ -375,21 +749,125 @@ const program = [
   },
 ];
 
-// ===============toggle the program to get all the workouts =============
-const programContainer = document.querySelectorAll(".program-container");
+// ===============display Programs  =================
+const programGridContainer = document.querySelector(
+  ".workouts-programs-grid-container"
+);
+const displayProgram = () => {
+  // =========displayin the program name and the tools and the arrow ,on click we show the program workouts then on other click an overview for the exercises
 
-programContainer.forEach(function (program) {
-  const programArrow = program.querySelector("#show-program");
+  programGridContainer.innerHTML = "";
+  for (let i = 0; i < programPlan.length; i++) {
+    programGridContainer.innerHTML += `<div class="program-container">
+ 
+  <div class="program">
+    <img
+      src="../images/Arrow up.svg"
+      id="show-program"
+      alt=""
+      data-overview= ${i}
+    />
+    <p>${programPlan[i].name}</p>
+    
+    <div class="tools">
+      <i class="fa-solid fa-trash" data-manage="programId"></i>
+      <i class="fa-regular fa-pen-to-square" data-delete="programId"></i>
+    </div>
+  </div>
+  
+  <div class="overview-container">
+  <div class="date-stats">
+  <p class="created-at">created at: 22/10/2022</p>
+  <p class="updated-at">updated at: 22/11/2022</p>
+</div>
+</div>
+</div>`;
+  }
+  // ===============toggle the program to get all the workouts =============
+  const programContainer = document.querySelectorAll(".program-container");
 
-  programArrow.addEventListener("click", function () {
-    programContainer.forEach(function (item) {
-      if (item === program) {
-        console.log(item);
-        item.classList.toggle("open-container");
-        programArrow.classList.toggle("rotate-arrow");
-      } else {
-        item.classList.remove("open-container");
-      }
+  programContainer.forEach(function (program) {
+    const programArrow = program.querySelector("#show-program");
+    const overviewContainer = program.querySelector(".overview-container");
+
+    programArrow.addEventListener("click", function (e) {
+      let index = e.target.dataset.overview;
+
+      programContainer.forEach(function (item) {
+        // =====opening container and displaying the workouts on click
+        if (item === program) {
+          overviewContainer.innerHTML = `<div class="date-stats">
+          <p class="created-at">created at: ${programPlan[index].createdAt}</p>
+          <p class="updated-at">updated at: ${programPlan[index].updatedAt}</p>
+        </div>`;
+          ``;
+          item.classList.toggle("open-container");
+
+          let workout = programPlan[index].workouts;
+          for (let i = 0; i < workout.length; i++) {
+            overviewContainer.innerHTML += `<div class="workout-container">
+            <div class="workout">
+              <img src="../images/Arrow up.svg" id="show-exercise" alt="" data-exercise = '${i}'  />
+              <p class="workout-name">${workout[i].name}</p>
+              <div class="tools">
+                <i class="fa-solid fa-trash"></i>
+                <i class="fa-regular fa-pen-to-square"></i>
+              </div>
+            </div>
+            
+            <div class="exercise-container">
+              </div>
+          </div>`;
+          }
+
+          // ========displaying th exercise on click ========
+          const workoutContainer =
+            document.querySelectorAll(".workout-container");
+
+          // =====workouts for each
+
+          workoutContainer.forEach((workout) => {
+            const showExerciseBtn = workout.querySelector("#show-exercise");
+            const exerciseContainer = workout.querySelector(
+              ".exercise-container"
+            );
+
+            showExerciseBtn.addEventListener("click", function (e) {
+              let workoutIndex = e.target.dataset.exercise;
+              console.log(workoutIndex);
+              workoutContainer.forEach((subWorkout) => {
+                if (subWorkout === workout) {
+                  subWorkout.classList.toggle("show-info");
+                  exerciseContainer.innerHTML = "";
+                  let exercise =
+                    programPlan[index].workouts[workoutIndex].exercises;
+
+                  for (let i = 0; i < exercise.length; i++) {
+                    exerciseContainer.innerHTML += `<div class="exercise">
+  <p class="exercise-name"><span></span> ${exercise[i].name}</p>
+  <div class="exercise-info">
+    <div class="exercise-stats">
+      <p class="sets stat-dot"><span></span> set: ${exercise[i].sets}</p>
+      <p class="reps stat-dot"><span></span> reps: ${exercise[i].Reps}</p>
+    </div>
+    <div class="exercise-stats">
+      <p class="tempo stat-dot"><span></span> tempo: ${exercise[i].tempo}</p>
+      <p class="rest-time stat-dot"><span></span> rest: ${exercise[i].restTime}</p>
+    </div>
+  </div>
+</div>`;
+                  }
+                } else {
+                  subWorkout.classList.remove("show-info");
+                }
+              });
+            });
+          });
+        } else {
+          item.classList.remove("open-container");
+        }
+      });
     });
   });
-});
+};
+displayProgram();
