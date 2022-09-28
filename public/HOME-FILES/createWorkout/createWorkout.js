e58be6ea3225d634329e6ed20dce6b6fd5c2ca66
ziad astih <ziadastih12@outlook.com>
2022-09-28 21:26:03 +0300

    started with create workout page , set up an array with few workouts later will get some images inside it and set the logic

---
+++ public/HOME-FILES/createWorkout/createWorkout.js
@@ -0,0 +1,53 @@
+const exercices = [
+  {
+    name: "Seated Alternating Hammer Curls",
+  },
+  {
+    name: "Rotating Alternate Incline Bicep Curls",
+  },
+  {
+    name: "Alternating Deltoid Raise",
+  },
+  {
+    name: "Standing Alternate Rotating Bicep Curls ",
+  },
+  {
+    name: "Alternating Dumbbell Floor Press ",
+  },
+  {
+    name: "Alternating Dumbbell Floor Press ",
+  },
+  {
+    name: "Alternating Front Raise",
+  },
+  {
+    name: "Alternating Preacher Curl",
+  },
+  {
+    name: "Seated Arnold Press",
+  },
+  {
+    name: "Seated Arnold Press",
+  },
+  {
+    name: "Around The Worlds",
+  },
+  {
+    name: "Bent-Arm Dumbbell Pullover",
+  },
+  {
+    name: "Standing Rear Delt Flyes Head Against Bench",
+  },
+  {
+    name: "Dumbbell Bent Over Rows",
+  },
+  {
+    name: "Dumbbell Bent Over Rows",
+  },
+  {
+    name: "Dumbbell Bent Over Rows Neutral Grip",
+  },
+  {
+    name: "Car Drivers",
+  },
+];
