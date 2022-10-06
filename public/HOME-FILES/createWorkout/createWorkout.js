f93dc3def24c7d7d6d7dfb8a6947c7eaef851ec9
ziad astih <ziadastih12@outlook.com>
2022-10-06 21:40:36 +0300

    done with program name creating , editing .... and now slowly building the steps toward creating a workout , need to set the name and edit btn for the workout section and the toggle the exercises and work with checkBox

--- public/HOME-FILES/createWorkout/createWorkout.js
+++ public/HOME-FILES/createWorkout/createWorkout.js
@@ -51,6 +51,108 @@ const exercices = [
     name: "Car Drivers",
   },
 ];
+// =====program schema array , it has name, weeks,by default we will set one week , with 7 days
+const program = [
+  {
+    name: "",
+    weeks: [
+      {
+        days: [
+          {
+            name: "monday",
+            workouts: [],
+          },
+          {
+            name: "tuesday",
+            workouts: [],
+          },
+          {
+            name: "wednesday",
+            workouts: [],
+          },
+          {
+            name: "thursday",
+            workouts: [],
+          },
+          {
+            name: "friday",
+            workouts: [],
+          },
+          {
+            name: "saturday",
+            workouts: [],
+          },
+          {
+            name: "sunday",
+            workouts: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+// ==============creating the program name and editing it =====
+const overlay = document.querySelector(".overlay");
+const createProgramNameContainer = document.querySelector(
+  ".create-program-name"
+);
+const programNameInput = document.querySelector("#program-input-name");
+const editProgramNameBtn = document.querySelector(".edit-program-name");
+const createProgramNameBtn = document.querySelector(".add-program-name");
+const programName = document.querySelector(".program-name-header");
+const editProgramNameIcon = document.querySelector(".edit-program-name-icon");
+const nameInputAlert = document.querySelector(".create-program-name-alert");
+
+// ===============event listener for adding program/ edit icon / and edit program name =============
+createProgramNameBtn.addEventListener("click", () => {
+  addProgramName();
+});
+
+editProgramNameIcon.addEventListener("click", () => {
+  createProgramNameContainer.classList.remove("display-none");
+  overlay.classList.remove("display-none");
+  programNameInput.value = programName.textContent;
+  editProgramNameIcon.classList.remove("show-opacity");
+  createProgramNameBtn.classList.add("display-none");
+  editProgramNameBtn.classList.add("display-flex");
+});
+
+editProgramNameBtn.addEventListener("click", () => {
+  addProgramName();
+});
+
+// toggle the create workout name container =========
+
+const mainContainer = document.querySelector(".main-container");
+
+const toggleWorkoutNameContainer = document.getElementById(
+  "toggle-workout-name-container"
+);
+const workoutNameContainer = document.querySelector(".create-workout-name");
+
+toggleWorkoutNameContainer.addEventListener("click", () => {
+  workoutNameContainer.classList.add("display-flex");
+  overlay.classList.remove("display-none");
+});
+
+// ============ add name and display different setup in main container , show exercises list and workout name with edit btn for it ==
+const days = document.querySelectorAll(".day");
+
+const workoutNameInput = document.getElementById("workout-input-name");
+const workoutCreateBtn = document.querySelector(".add-workout-name");
+const workoutEditBtn = document.querySelector(".edit-workout-name");
+const workoutNameAlert = document.querySelector(".workout-name-alert");
+
+workoutCreateBtn.addEventListener("click", () => {
+  if (workoutNameInput.value.length === 0) {
+    showAlert(workoutNameAlert);
+  } else {
+    console.log("hello");
+  }
+});
+
+// ========displaying the exercices we have inside the exercises list container
 
 const exercicesListContainer = document.querySelector(
   ".exercises-list-container"
@@ -72,3 +174,45 @@ const displayExercicesArray = () => {
 };
 
 displayExercicesArray();
+
+// =======================show alert function ===========================
+const showAlert = async (alert) => {
+  await alert.classList.add("display-flex");
+  setTimeout(() => {
+    alert.classList.remove("display-flex");
+  }, 3000);
+};
+
+const addProgramName = () => {
+  if (programNameInput.value.length === 0) {
+    showAlert(nameInputAlert);
+  } else {
+    programName.textContent = programNameInput.value;
+    editProgramNameIcon.classList.add("show-opacity");
+    overlay.classList.add("display-none");
+    createProgramNameContainer.classList.add("display-none");
+    program.name = programNameInput.value;
+    console.log(program.name);
+  }
+};
+
+// ========== display workouts =====================
+
+{
+  /* <div class="one-workout">
+      <i class="fa-solid fa-list" id="show-program" data-execises="s"></i>
+      <p class="workout-name">split bros</p>
+      <div class="tools">
+        <i
+          class="fa-solid fa-trash"
+          id="delete-workout"
+          data-delete="workout-index"
+        ></i>
+        <i
+          class="fa-solid fa-user-pen"
+          id="manage-workout"
+          data-manage="workout-index"
+        ></i>
+      </div>
+    </div>  */
+}
