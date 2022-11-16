0b456e729fe7fb05efd4871173b8a0caf7c8e4b3
ziad astih <ziadastih12@outlook.com>
2022-11-16 12:28:34 +0200

    done with client homepage need to test it a little and then move to designing nutrition oage

--- public/HOME-FILES/clientHomepage/clientHomepage.js
+++ public/HOME-FILES/clientHomepage/clientHomepage.js
@@ -4,9 +4,6 @@ const clientId = localStorage.getItem("cref");
 
 // ===============client name and displaying the general coach info ========
 const clientName = document.querySelector(".client-name");
-const clientProfile = document.querySelector(".profile-pic");
-const clientNameContainer = document.querySelector(".client-name-container");
-const coachName = document.querySelector(".coach-name");
 const coachEmailBtn = document.querySelector("#email-icon");
 const coachWhatsappBtn = document.querySelector("#whatsapp-icon");
 
@@ -25,17 +22,15 @@ const getClient = async () => {
     const {
       data: { coach },
     } = await axios.get(`/api/v1/coach/${coachId}`);
-    const coachfirstName = coach.coachFirstName;
-    const coachlastName = coach.coachLastName;
+
     const coachEmail = coach.email;
     const coachNumber = coach.number;
 
-    clientProfile.textContent = `${clientFirstName.slice(0, 1).toUpperCase()}`;
     clientName.innerHTML = `${clientFirstName} ${clientLastName}`;
-    coachName.innerHTML = `${coachfirstName} ${coachlastName}`;
+
     coachEmailBtn.href = `mailto:${coachEmail}`;
     clientName.classList.add("show-opacity");
-    coachName.classList.add("show-opacity");
+
     if (coachNumber) {
       coachWhatsappBtn.href = `https://wa.me/${coachNumber}`;
     } else {
@@ -48,7 +43,68 @@ const getClient = async () => {
 
 getClient();
 
-const logoutBtn = document.getElementById("user-logout-nav-btn");
+// =============dashboard update  =========================
+const progressCircle = document.querySelector(".circular-progress");
+const totalCalories = document.querySelector(".total-calories");
+const currentCalories = document.querySelector(".current-calories");
+const carbsValue = document.querySelector(".carbs-value");
+const carbsProgressBar = document.querySelector("#carbs-bar");
+const proteinValue = document.querySelector(".protein-value");
+const proteinBar = document.querySelector("#protein-bar");
+const fatValue = document.querySelector(".fat-value");
+const fatBar = document.querySelector("#fat-bar");
+const workoutValue = document.querySelector(".workout-value");
+const workoutsBr = document.querySelector("#workout-bar");
+const getDailyMacros = async () => {
+  try {
+    const dailyMacros = await axios.get(`/api/v1/dailyMacros/${clientId}`);
+    console.log(dailyMacros);
+    let maxCalories = parseFloat(
+      dailyMacros.data.dailyMacros.totalMacros.calories
+    );
+    let maxProt = parseFloat(dailyMacros.data.dailyMacros.totalMacros.protein);
+    let maxCarbs = parseFloat(dailyMacros.data.dailyMacros.totalMacros.carbs);
+    let maxFat = parseFloat(dailyMacros.data.dailyMacros.totalMacros.fat);
+    let maxWorkouts = dailyMacros.data.dailyMacros.totalWorkouts;
+
+    let currentCal = dailyMacros.data.dailyMacros.currentMacros.calories;
+    let currentCarbs = dailyMacros.data.dailyMacros.currentMacros.carbs;
+    let currentProt = dailyMacros.data.dailyMacros.currentMacros.prot;
+    let currentFat = dailyMacros.data.dailyMacros.currentMacros.fat;
+    let currentWorkouts = dailyMacros.data.dailyMacros.currentWorkouts;
+    totalCalories.textContent = `${maxCalories} kcal`;
+    currentCalories.textContent = `${currentCal}`;
+
+    carbsValue.textContent = `${currentCarbs} / ${maxCarbs}g`;
+    proteinValue.textContent = `${currentProt} / ${maxProt}g`;
+    fatValue.textContent = `${currentFat} / ${maxFat}g`;
+    workoutValue.textContent = `${currentWorkouts} / ${maxWorkouts}`;
+    if (maxCalories > 0) {
+      let circleDeg = parseFloat((currentCal * 100) / maxCalories) * 3.6;
+      progressCircle.style.background = `conic-gradient(var(--blue) ${circleDeg}deg, var(--white) 0deg)`;
+    }
+    let carbsWidth = ((currentCarbs * 100) / maxCarbs) * 1.5;
+    let protWidth = ((currentProt * 100) / maxProt) * 1.5;
+    let fatWidth = ((currentFat * 100) / maxFat) * 1.5;
+    let workoutWidth = ((currentWorkouts * 100) / maxWorkouts) * 1.5;
+    carbsProgressBar.style.width = `${carbsWidth}px`;
+    proteinBar.style.width = `${protWidth}px`;
+    fatBar.style.width = `${fatWidth}px`;
+    workoutsBr.style.width = `${workoutWidth}px`;
+  } catch (error) {
+    let msg = `no dailyMacros with id ${clientId}`;
+    if (msg === error.response.data.msg) {
+      const dailyMacros = await axios.post(`/api/v1/dailyMacros`, {
+        createdFor: clientId,
+      });
+    } else {
+      return;
+    }
+  }
+};
+getDailyMacros();
+
+const logoutBtn = document.getElementById("logout-btn");
 
 logoutBtn.addEventListener("click", async () => {
   try {
